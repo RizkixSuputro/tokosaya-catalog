f7a889773f7bc52fb124bce6eeb9d6ecbe938776
rizkiusername <buildporterkii@gmail.com>
2026-09-17 16:12:43 +0800

    Update route : Menyesuaikan route yang ada di halaman home dan daftar produk dengan controller

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProdukController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('user-front.home');
-});
+Route::get('/', [HomeController::class, 'home'])->name('home');
+Route::get('/kontak', [HomeController::class, 'kontak']);
+
+Route::get('/produk', [ProdukController::class, 'index'])->name('daftar-produk');
+Route::get('/produk/{id}', [ProdukController::class, 'show'])->name('detail-produk');
+
+Route::get('/kontak', function () {
+    return view('user-front.kontak');
+})->name('kontak');
 
 Route::get('/admin', function () {
     return view('admin.dashboard');
